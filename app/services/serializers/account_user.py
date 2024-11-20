fee8167dc53c97f77556275bb22f5ee090a1cbb5
Pham Ha <phamtuanha21092003@gmail.com>
2024-11-20 18:13:27 +0700

    update SerializerAccountUser

--- app/services/serializers/account_user.py
+++ app/services/serializers/account_user.py
@@ -8,6 +8,7 @@ class SerializerAccountUser(BaseSerializer):
     email = fields.String()
     is_active = fields.Boolean()
     avatar = fields.String()
+    last_login = fields.DateTime('%Y-%m-%d %H:%M:%S+00:00')
     is_friend = fields.Method("get_is_friend")
 
 
